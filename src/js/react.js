8cbb7a5b62c38bb4042991b7f7de70f5c09ddea0
kevin940726 <kevin830726@gmail.com>
2016-07-04 00:05:47 +0800

    STEP 3: put duplicate todos into state and map through them

--- src/js/react.js
+++ src/js/react.js
@@ -2,67 +2,75 @@
 import React from 'react';
 import '../css/todo.global.css';
 
-const TodoApp = () => (
-	<div className="box">
-		<div className="tabs is-centered">
-			<ul>
-				<li className="is-active"><a>All</a></li>
-				<li><a>Pending</a></li>
-				<li><a>Done</a></li>
-			</ul>
-		</div>
+class TodoApp extends React.Component {
+	constructor() {
+		super();
 
-		<div className="content">
-			<p className="control">
-				<label className="checkbox">
-					<input type="checkbox" checked />
-					Work it harder.
-					<span className="icon is-small">
-						<i className="fa fa-times"></i>
-					</span>
-				</label>
-			</p>
+		this.state = {
+			todos: [
+				{
+					id: '1',
+					text: 'Work it harder.',
+					isChecked: true,
+				},
+				{
+					id: '2',
+					text: 'Make it better.',
+					isChecked: false,
+				},
+				{
+					id: '3',
+					text: 'Do it faster.',
+					isChecked: false,
+				},
+				{
+					id: '4',
+					text: 'Makes us stronger.',
+					isChecked: false,
+				},
+			],
+		};
+	}
 
-			<p className="control">
-				<label className="checkbox">
-					<input type="checkbox" />
-					Make it better.
-					<span className="icon is-small">
-						<i className="fa fa-times"></i>
-					</span>
-				</label>
-			</p>
+	render() {
+		return (
+			<div className="box">
+				<div className="tabs is-centered">
+					<ul>
+						<li className="is-active"><a>All</a></li>
+						<li><a>Pending</a></li>
+						<li><a>Done</a></li>
+					</ul>
+				</div>
 
-			<p className="control">
-				<label className="checkbox">
-					<input type="checkbox" />
-					Do it faster.
-					<span className="icon is-small">
-						<i className="fa fa-times"></i>
-					</span>
-				</label>
-			</p>
+				<div className="content">
+					{this.state.todos.map(todo => (
+						<p key={todo.id} className="control">
+							<label className="checkbox">
+								<input
+									type="checkbox"
+									checked={todo.isChecked}
+								/>
+								{todo.text}
+								<span className="icon is-small">
+									<i className="fa fa-times"></i>
+								</span>
+							</label>
+						</p>
+					))}
+				</div>
 
-			<p className="control">
-				<label className="checkbox">
-					<input type="checkbox" />
-					Makes us stronger.
-					<span className="icon is-small">
-						<i className="fa fa-times"></i>
-					</span>
-				</label>
-			</p>
-		</div>
-
-		<form className="control">
-			<input
-				id="input-todo"
-				className="input"
-				type="text"
-				placeholder="Add some todos..."
-			/>
-		</form>
-	</div>
-);
+				<form className="control">
+					<input
+						id="input-todo"
+						className="input"
+						type="text"
+						placeholder="Add some todos..."
+					/>
+				</form>
+			</div>
+		);
+	}
+}
 
 export default TodoApp;
