1ac1cbf580d6d4e1fe756b9b1e9f812db8eb892b
kevin940726 <kevin830726@gmail.com>
2016-07-03 19:32:12 +0800

    add default cursor

--- step-by-step.js
+++ step-by-step.js
@@ -1,57 +1,63 @@
 import inquirer from 'inquirer';
 import shelljs from 'shelljs';
 
-const questions = [
-	{
-		type: 'list',
-		name: 'step',
-		message: 'Choose a step to jump to.',
-		choices: [
-			{
-				name: 'Step 1',
-				value: {
-					step: '1',
-					hash: 'f666178b01bbc1aee6acb31d2ac98065711c2b2c',
-				},
+const question = {
+	type: 'list',
+	name: 'step',
+	message: 'Choose a step to jump to.',
+	choices: [
+		{
+			name: 'Step 1',
+			value: {
+				index: 0,
+				step: '1',
+				hash: 'f666178b01bbc1aee6acb31d2ac98065711c2b2c',
 			},
-			{
-				name: 'Step 2',
-				value: {
-					step: '2',
-					hash: 'ae5c5b486ec4952ff17a94b4f40753868b7aa280',
-				},
+		},
+		{
+			name: 'Step 2',
+			value: {
+				index: 1,
+				step: '2',
+				hash: 'ae5c5b486ec4952ff17a94b4f40753868b7aa280',
 			},
-			{
-				name: 'Step 3',
-				value: {
-					step: '3',
-					hash: '6b00fccb9b466f43036e0783d32ae8b2aefb8d9d',
-				},
+		},
+		{
+			name: 'Step 3',
+			value: {
+				index: 2,
+				step: '3',
+				hash: '6b00fccb9b466f43036e0783d32ae8b2aefb8d9d',
 			},
-			{
-				name: 'Final step and exit',
-				value: {
-					step: 'final',
-					hash: 'master',
-				},
+		},
+		{
+			name: 'Final step and exit',
+			value: {
+				index: 3,
+				step: 'final',
+				hash: 'master',
 			},
-		],
-	},
-];
-
-const prompt = () => {
-	inquirer.prompt(questions)
-		.then(answers => {
-			shelljs.exec(
-				`git checkout -f ${answers.step.hash}`,
-				{ silent: true }
-			);
-
-			console.log(`You are now in step ${answers.step.step}`);
-			if (answers.step.step !== 'final') {
-				prompt();
-			}
-		});
+		},
+	],
 };
 
-prompt();
+const prompt = defaultStep => {
+	inquirer.prompt([{
+		...question,
+		default: defaultStep,
+	}]).then(answers => {
+		const answer = answers.step;
+
+		shelljs.exec(
+			`git checkout -f ${answer.hash}`,
+			{ silent: true }
+		);
+
+		console.log(`You are now in step ${answer.step}`);
+		if (answer.step !== 'final') {
+			prompt(answer.index);
+		}
+	});
+};
+
+prompt(0);
