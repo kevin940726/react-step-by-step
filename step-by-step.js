e0e161643756067e05a95e1130c8ff630e05b3df
kevin940726 <kevin830726@gmail.com>
2016-07-03 19:25:43 +0800

    additional step msg

--- step-by-step.js
+++ step-by-step.js
@@ -9,19 +9,31 @@ const questions = [
 		choices: [
 			{
 				name: 'step 1',
-				value: 'f666178b01bbc1aee6acb31d2ac98065711c2b2c',
+				value: {
+					step: '1',
+					hash: 'f666178b01bbc1aee6acb31d2ac98065711c2b2c',
+				},
 			},
 			{
 				name: 'step 2',
-				value: 'ae5c5b486ec4952ff17a94b4f40753868b7aa280',
+				value: {
+					step: '2',
+					hash: 'ae5c5b486ec4952ff17a94b4f40753868b7aa280',
+				},
 			},
 			{
 				name: 'step 3',
-				value: '6b00fccb9b466f43036e0783d32ae8b2aefb8d9d',
+				value: {
+					step: '3',
+					hash: '6b00fccb9b466f43036e0783d32ae8b2aefb8d9d',
+				},
 			},
 			{
 				name: 'final step',
-				value: 'master',
+				value: {
+					step: 'final',
+					hash: 'master',
+				},
 			},
 		],
 	},
@@ -30,12 +42,12 @@ const questions = [
 const prompt = () => {
 	inquirer.prompt(questions)
 		.then(answers => {
-			const msg = shelljs.exec(
-				`git checkout ${answers.step}`,
+			shelljs.exec(
+				`git checkout -f ${answers.step.hash}`,
 				{ silent: true }
-			).stdout;
+			);
 
-			console.log(msg);
+			console.log(`You are now in step ${answers.step.step}`);
 			prompt();
 		});
 };
