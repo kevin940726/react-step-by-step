961f990f3e002ec33274d50fb3d92a8c5fa2411f
kevin940726 <kevin830726@gmail.com>
2016-07-09 19:58:20 +0800

    update instruction

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# React tutorial
+## Step 4
+> In this step: add todo event handler, check and remove
 
-- test commit 1
-- test commit 2
-- test commit 3
+Create our event handler by making corresponding function in our component. Be sure to bind them in the `constructor()` or we will not be able to access them in the `render()` function.
+
+We create a new function in the element event handler for each todo, and pass the corresponding id to the function. Though it may affect the performance, but let's not worry about it now.
+
+In each event function, we call the `setState` method to replace the current state with our new state. State cannot be directly modified, in other words, `this.state.todos = newTodos` is forbidden. We have to call the `setState()` function in order to change our state.
+
+It is highly recommended that we use the _functional programming_ way (aka higher-order function) to modify our state. Simply said, we should not call any function that will modify the original state (i.e. `push()`, `pop()`), instead, we should always return a new state after we made any modification.
+
+Now, our app can check / uncheck the todo, and also removing them. In the next step, we will learn how to add a new one.
--- src/js/react.js
+++ src/js/react.js
@@ -31,21 +31,34 @@ class TodoApp extends React.Component {
 			],
 		};
 
+		/* bind your function to this component,
+		 * quite fixed usage, just remember to do it after you create a function */
 		this.handleCheck = this.handleCheck.bind(this);
 		this.handleRemove = this.handleRemove.bind(this);
 	}
 
+	/* create a check event handler for every todo,
+	 * it will fire whenever user check / uncheck the checkbox */
 	handleCheck(e, id) {
+		/* create a clone of the todos array,
+		 * it's a good practice to never modify the state,
+		 * you can do this any other way though (functional programming is recommended) */
 		const newTodos = this.state.todos.slice();
 		newTodos.find(todo => todo.id === id).isChecked = e.target.checked;
 
+		/* set the state by calling this.setState(),
+		 * it will replace the current state with the new one */
 		this.setState({
 			todos: newTodos,
 		});
 	}
+	/* same as handleCheck(), handle remove of each todo */
 	handleRemove(e, id) {
 		e.stopPropagation();
 
+		/* this time we do this in a functional programming way,
+		 * filter will return a new array with only items that satisfy the function,
+		 * in this case, it will return a new array without the particular todo */
 		this.setState({
 			todos: this.state.todos.filter(todo => todo.id !== id),
 		});
@@ -66,6 +79,8 @@ class TodoApp extends React.Component {
 					{this.state.todos.map(todo => (
 						<p key={todo.id} className="control">
 							<label className="checkbox">
+								{/* inject a onChange or onClick event listener to the element,
+									* pass the id to the function to map to every todos */}
 								<input
 									type="checkbox"
 									checked={todo.isChecked}
